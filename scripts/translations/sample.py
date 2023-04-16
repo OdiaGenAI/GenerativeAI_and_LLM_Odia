2f147debc24c22ef5a0928a8b43bd97a082f3acb
Swateek Jena <swateek@gmail.com>
2023-04-16 23:48:05 +0530

    #4 - Checking in sample script

--- scripts/translations/sample.py
+++ scripts/translations/sample.py
@@ -6,7 +6,7 @@ from inference.engine import Model
 en2indic_exp_dir = "../en-indic"
 en2indic_model = Model(expdir=en2indic_exp_dir)
 
-sents = ["hello world"]
+sents = ["Jai Jagannath!"]
 # en2indic_model.batch_translate(sents, "en", "hi")
 odia_splits = en2indic_model.batch_translate(sents, "en", "or")
 response = "\n".join(odia_splits)
